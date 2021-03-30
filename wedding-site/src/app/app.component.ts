9cb6cc3f5317ebf3eb53703ef56c7e1c4ce439d9
manojmpg114 <manojmpg114@aol.com>
2021-03-30 16:12:40 -0400

    Search model addition
    Current state where local dataset can be populated and searched ; ngmodel + input adjustments

--- wedding-site/src/app/app.component.ts
+++ wedding-site/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wedding-site';
+  dataset = ['Manoj George: Table 1', 'Alana Roy: Table 1', 'Siby Chechi: Center Stage', 'Paco: Kitchen'];
+  searchText: string ="";
 }
