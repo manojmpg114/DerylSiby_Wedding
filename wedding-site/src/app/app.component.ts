28b92100361a717b31ce727c70052aa7123fdbf3
manojmpg114 <manojmpg114@aol.com>
2021-04-03 16:17:33 -0400

    filter adjustment + table0
    working set for list search and table map
    
    editions with conditional ngif and multiple table images ongoing

--- wedding-site/src/app/app.component.ts
+++ wedding-site/src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wedding-site';
-  dataset = ['Manoj George: Table 1', 'Alana Roy: Table 1', 'Siby Chechi: Center Stage', 'Paco: Kitchen'];
+  dataset = ['Manoj George: Table 1', 'Alana Roy: Table 1',
+   'Siby Chechi: Center Stage', 'Paco: Kitchen', 'Wonka: Under Table',
+  'Larry Lobster: Under The Sea', 'Bessie Chechi: Table 2'];
   searchText: string ="";
 }
