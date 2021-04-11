efdd84dc909504ea888fe2f3343650d91a53a002
manojmpg114 <manojmpg114@aol.com>
2021-04-11 15:32:05 -0400

    splash screen left trans
    issues configuring multiple inputs of transition ** adjustments on ngif or switch

---
+++ wedding-site/src/app/splash-screen/splash-animation-type.ts
@@ -0,0 +1,5 @@
+export enum SplashAnimationType {
+    SlideLeft = 'slide-left',
+    SlideRight = 'slight-right',
+    FadeOut = 'fade-out'
+}
--- wedding-site/src/app/splash-screen/splash-screen.component.ts
+++ wedding-site/src/app/splash-screen/splash-screen.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { SplashAnimationType } from './splash-animation-type';
 
 @Component({
   selector: 'app-splash-screen',
@@ -8,9 +9,67 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class SplashScreenComponent implements OnInit {
 
+  windowWidth?: string;
+  showSplash: boolean = true;
+  opacityChange = 1;
+  splashTransition?: string;
+
+  @Input() animationDuration: number = 0.5;
+  @Input() duration: number = 3;
+  @Input() animationType: SplashAnimationType = SplashAnimationType.SlideLeft;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    /*
+    
+    setTimeout( () => {
+
+      let transitionStyle = '';
+
+      switch(this.animationType){
+        case SplashAnimationType.SlideLeft:
+          this.windowWidth = '-' + window.innerWidth + 'px';
+          transitionStyle = 'left' + this.animationDuration + 's';
+          break;
+        case SplashAnimationType.SlideRight:
+          this.windowWidth = window.innerWidth + 'px';
+          transitionStyle = 'left' + this.animationDuration + 's';
+          break;
+        case SplashAnimationType.FadeOut:
+          
+          transitionStyle = 'opacity' + this.animationDuration + 's';
+          this.opacityChange = 0;
+          break;
+        default:
+          this.windowWidth = '-' + window.innerWidth + 'px';
+          transitionStyle = 'left' + this.animationDuration + 's';
+          break;
+      }
+
+      this.splashTransition = transitionStyle;
+
+      setTimeout(() => {
+
+        
+        
+      }, this.animationDuration * 1000);
+
+      this.showSplash = !this.showSplash;
+
+    }, this.duration * 1000);
+    */
+
+    /*this.windowWidth = '-' + window.innerWidth + 'px';*/
+    
+    setTimeout(() => {
+      this.windowWidth = '-' + window.innerWidth + 'px';
+      setTimeout(() => {
+        this.showSplash = !this.showSplash;
+      }, 500)
+    }, 3000)
+
   }
 
 }
